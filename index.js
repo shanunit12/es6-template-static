172294151cea0a0dc223f71ce08e8fbfdbe1de33
shanunit12 <72335315+shanunit12@users.noreply.github.com>
2020-12-10 18:27:49 -0500

    change

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const Page = require("./_layout/Default");
 module.exports = class extends Page {
     constructor(){
         super({title:"Home", sName:"Shannon Halycz"});
-    }
+         }
     render(sPage) {
         return `
         ${this.requireMarked('_pages/index.md')}
         `;
         
+        
     }
+    {this.sUrlToEmailer = "https://tranquil-badlands-69388.herokuapp.com/send/";}
    
 }
--- js/main.js
+++ js/main.js
@@ -1,15 +1,8 @@
 
-  
-import "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js";
-import "https://cdnjs.cloudflare.com/ajax/libs/marked/1.1.1/marked.js";
-import "https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js";
-import aPages from "../pages/index.js";
-import aItems from "../items/index.js";
-
 class Page {
     constructor(){
         this.sName = "Shannon Halycz";
-        this.sUrlToEmailer = "https://tranquil-badlands-69388.herokuapp.com/send";
+        this.sUrlToEmailer = "https://tranquil-badlands-69388.herokuapp.com/send/";
         const sBase = document.location.pathname;
         if(sBase[sBase.length - 1] == "/"){
             this.sBase = sBase.substr(0, sBase.length -1);
